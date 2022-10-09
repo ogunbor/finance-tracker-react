6b18ab50293ff44b7739f6480dfbea43203594f8
Jerry Ogunbor <ogunborjerry@gmail.com>
2022-10-09 14:39:37 +0100

    Worked on Signup.js

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,6 +1,52 @@
+import { useState } from "react";
+
 // styles
 import styles from "./Signup.module.css";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, displayName);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles["signup-form"]}>
+      <h2>sign up</h2>
+      <label>
+        <span>username:</span>
+        <input
+          type="text"
+          minLength={4}
+          maxLength={10}
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+          required
+        />
+      </label>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          required
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          required
+        />
+      </label>
+
+      <button className="btn">Sign up</button>
+    </form>
+  );
 }
