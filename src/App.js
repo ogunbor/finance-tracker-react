922272e16bf7ca1aa7eb824bfdc9afcc0beff7f9
Jerry Ogunbor <ogunborjerry@gmail.com>
2022-10-09 13:56:19 +0100

    created navbar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Signup from "./pages/signup/Signup";
 import Login from "./pages/login/Login";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
