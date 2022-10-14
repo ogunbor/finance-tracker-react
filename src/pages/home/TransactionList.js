8bb4f21d8281e0e28d85b12f8e837db4a0d5dbd8
Jerry Ogunbor <ogunborjerry@gmail.com>
2022-10-14 13:14:56 +0100

    see readme

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -1,13 +1,17 @@
+import { useFirestore } from "../../hooks/useFirestore";
+
 // styles
 import styles from "./Home.module.css";
 
 export default function TransactionList({ transactions }) {
+  const { deleteDocument } = useFirestore("transactions");
   return (
     <ul className={styles.transactions}>
       {transactions.map((transaction) => (
         <li key={transaction.id}>
           <p className={styles.name}>{transaction.name}</p>
           <p className={styles.amount}>&#8358;{transaction.amount}</p>
+          <button onClick={() => deleteDocument(transaction.id)}>x</button>
         </li>
       ))}
     </ul>
